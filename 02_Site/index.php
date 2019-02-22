3dbe3b76ba1f9166ca9fae482e67e238c63e509d
ibrah91 <47572340+ibrah91@users.noreply.github.com>
2019-02-22 17:01:12 +0100

    version initiale

---
+++ 02_Site/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>02 Site</title>
+
+ <link rel="shortcut icon" href="http://php.net/favicon.ico">
+ <link rel="stylesheet" type ="text/css" href="style.css">
+</head>
+<body>
+
+  <h1>
+    Mon site
+
+  </h1>
+</body>
+</html>
